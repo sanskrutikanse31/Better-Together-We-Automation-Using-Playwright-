d4f2cf325a8fce1d0d2c427f36345660dcb34b84
Sanskruti Kanse <121843639+sanskrutikanse31@users.noreply.github.com>
2026-06-22 16:01:47 +0530

    Comment out login and registration test steps
    Comment out multiple lines related to the login process and registration steps.

--- tests/TC001_Login.spec.ts
+++ tests/TC001_Login.spec.ts
@@ -17,38 +17,38 @@ test('test', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Enter password' }).fill('password1');
 
 
-  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.locator("(//span[contains(text(),'Sign In')])[1]").click();
 
 
   //************
-  await expect(page.getByRole('link', { name: 'Company logo' })).toBeVisible();
+  // await expect(page.getByRole('link', { name: 'Company logo' })).toBeVisible();
 
-  await page.getByText('Project-Angular 20 Testing').click();
+  // await page.getByText('Project-Angular 20 Testing').click();
 
-  await page.waitForTimeout(3000);
+  // await page.waitForTimeout(3000);
 
-  await page.getByRole('button', { name: 'Complete Registration' }).click();
-  await expect(page.getByRole('button', { name: 'go-back Go Back' })).toBeVisible();
+  // await page.getByRole('button', { name: 'Complete Registration' }).click();
+  // await expect(page.getByRole('button', { name: 'go-back Go Back' })).toBeVisible();
 
-  await page.getByRole('button', { name: 'Continue' }).click();
-  await expect(page.getByRole('button', { name: 'Add New Language' })).toBeVisible();
+  // await page.getByRole('button', { name: 'Continue' }).click();
+  // await expect(page.getByRole('button', { name: 'Add New Language' })).toBeVisible();
 
-  await page.locator('#mat-select-value-8').click();
-  await expect(page.getByRole('option', { name: 'Pan Card' })).toBeVisible();
+  // await page.locator('#mat-select-value-8').click();
+  // await expect(page.getByRole('option', { name: 'Pan Card' })).toBeVisible();
 
-  await page.getByRole('option', { name: 'Ration Card' }).click();
-  await page.getByRole('button', { name: 'Upload your ID Proof File' }).click();
-  await page.getByRole('button', { name: 'Upload your ID Proof File' }).setInputFiles('Event-Angular 20 Testing.pdf');
-  await expect(page.getByRole('button', { name: 'Remove' })).toBeVisible();
+  // await page.getByRole('option', { name: 'Ration Card' }).click();
+  // await page.getByRole('button', { name: 'Upload your ID Proof File' }).click();
+  // await page.getByRole('button', { name: 'Upload your ID Proof File' }).setInputFiles('Event-Angular 20 Testing.pdf');
+  // await expect(page.getByRole('button', { name: 'Remove' })).toBeVisible();
 
-  await page.locator('#mat-select-value-25').click();
-  await expect(page.getByRole('option', { name: 'Ahirani' })).toBeVisible();
+  // await page.locator('#mat-select-value-25').click();
+  // await expect(page.getByRole('option', { name: 'Ahirani' })).toBeVisible();
 
-  await page.getByRole('option', { name: 'Danish' }).click();
-  await expect(page.getByRole('combobox', { name: 'Danish' })).toBeVisible();
+  // await page.getByRole('option', { name: 'Danish' }).click();
+  // await expect(page.getByRole('combobox', { name: 'Danish' })).toBeVisible();
 
-  await page.getByRole('radio', { name: 'No' }).check();
-  await page.getByRole('textbox', { name: 'Website*' }).click();
-  await page.getByRole('textbox', { name: 'Website*' }).fill('www.hdfc.com');
-  await page.getByRole('button', { name: 'Continue' }).click();
+  // await page.getByRole('radio', { name: 'No' }).check();
+  // await page.getByRole('textbox', { name: 'Website*' }).click();
+  // await page.getByRole('textbox', { name: 'Website*' }).fill('www.hdfc.com');
+  // await page.getByRole('button', { name: 'Continue' }).click();
 });
